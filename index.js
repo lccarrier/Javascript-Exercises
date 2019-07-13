ec61499743acab2f2a3e1761c59f31e4d632e597
LC Carrier <lc@LCs-MacBook-Pro.local>
2019-07-13 18:00:24 -0400

    updated index.js with problem 9

--- index.js
+++ index.js
@@ -39,3 +39,14 @@ function isLeapYear(currentYear) {
 }
 console.log(isLeapYear(2020));
 
+
+// 9. Write a JavaScript program to calculate days left until next Christmas. 
+    var today = new Date();
+    var xmas = new Date(today.getFullYear(), 11, 25);
+
+    if (today.getMonth()==11 && today.getDate() > 25) {
+      xmas.setFullYear(xmas.getFullYear()+1);
+    }
+    var one_day = 1000 * 60 * 60 * 24;
+    console.log(Math.ceil((xmas.getTime()-today.getTime()) / (one_day)) + " days left until Christmas!");
+
